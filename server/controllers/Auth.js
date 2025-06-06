1645e074250b8ef06e040afe3035239dc942acbf
ayush kumar <kumarayush9527@gmail.com>
2025-06-06 20:37:07 +0530

    authcontroller updated

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -160,6 +160,7 @@ exports.login = async (req, res) => {
       res.cookie("token", token, options).status(200).json({
         success: true,
         token,
+        secure: true, 
         user,
         message: `User Login Success`,
       })
